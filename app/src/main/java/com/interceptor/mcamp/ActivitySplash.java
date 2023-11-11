5ab95983d0289a78bc66e32eaf6bf4c8bfd2a800
LTChanu <tchanu210@gmail.com>
2023-11-11 14:16:03 +0530

    First UI

---
+++ app/src/main/java/com/interceptor/mcamp/ActivitySplash.java
@@ -0,0 +1,18 @@
+package com.interceptor.mcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    public void openWelcome(View view) {
+    }
+}
