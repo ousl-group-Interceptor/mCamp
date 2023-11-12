4eaafc4d452a31764b736074dee99dd4ccc0d167
LTChanu <tchanu210@gmail.com>
2023-11-12 14:00:01 +0530

    UI correction

--- app/src/main/java/com/interceptor/mcamp/ActivityNotification.java
+++ app/src/main/java/com/interceptor/mcamp/ActivityNotification.java
@@ -1,21 +1,12 @@
 package com.interceptor.mcamp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Objects;
 
 public class ActivityNotification extends AppCompatActivity {
 
