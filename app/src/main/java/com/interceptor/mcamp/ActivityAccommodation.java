c91433ffcdbe7f68edf73066389518b5151983dd
IRESHA <maheshinaween@gmail.com>
2023-11-17 01:56:27 +0530

    feedback

---
+++ app/src/main/java/com/interceptor/mcamp/ActivityAccommodation.java
@@ -0,0 +1,14 @@
+package com.interceptor.mcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAccommodation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_accommodation);
+    }
+}
